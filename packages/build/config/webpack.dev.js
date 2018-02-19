8a1a8fa2baf766461f0d4a41df94fce16d464b0f
Christopher Cortes <christophercr@gmail.com>
2018-02-19 19:41:36 +0100

    feat(routing): adapt CSP style-src directive to allow inline styles from UI Router visualizer
    Not used for now since the style-src directive is commented due to other issues with Angular

--- packages/build/config/webpack.dev.js
+++ packages/build/config/webpack.dev.js
@@ -55,7 +55,7 @@ module.exports = function () {
     "object-src 'self'",
     "plugin-types application/pdf",  // valid mime-types for plugins invoked via <object> and <embed>
     // "script-src 'self'", // FIXME: enable as soon as the issue is fixed in Angular (https://github.com/angular/angular-cli/issues/6872 )
-    // "style-src 'self' 'nonce-cef324d21ec5483c8819cc7a5e33c4a2'" // we define the same nonce value as in the style-loader // FIXME: DomSharedStylesHost.prototype._addStylesToHost in platform-browser.js adds inline style!
+    // "style-src 'self' 'nonce-uiroutervisualizer' 'nonce-cef324d21ec5483c8819cc7a5e33c4a2'" // we define the same nonce value as in the style-loader // FIXME: DomSharedStylesHost.prototype._addStylesToHost in platform-browser.js adds inline style!
   ];
 
   return webpackMerge(commonConfig({ENV: ENV, metadata: METADATA}), {
