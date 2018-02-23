857b9732b6b64decdb0646d7e282ee32b28a2621
tenretC <ciroco@live.be>
2018-02-23 13:38:38 +0100

    package and add specification for .json files in .editorconfig

--- dist/packages/stark-build/config/webpack.common-data.js
+++ dist/packages/stark-build/config/webpack.common-data.js
@@ -11,20 +11,20 @@ const starkAppConfig = require(helpers.root("src/stark-app-config.json"));
 
 // PostCSS Plugins
 const postcssPlugins = [
-  // reference: https://github.com/postcss/postcss-import
-  // https://github.com/postcss/postcss-import/issues/244
-  require("postcss-import")(),
+	// reference: https://github.com/postcss/postcss-import
+	// https://github.com/postcss/postcss-import/issues/244
+	require("postcss-import")(),
 
-  // plugin to rebase, inline or copy on url().
-  // https://github.com/postcss/postcss-url
-  require("postcss-url")(),
+	// plugin to rebase, inline or copy on url().
+	// https://github.com/postcss/postcss-url
+	require("postcss-url")(),
 
-  require("postcss-nesting")(),
-  require("postcss-simple-extend")(),
-  require("postcss-cssnext")({
-    // see https://github.com/MoOx/postcss-cssnext/issues/268 for example
-    browsers: ["last 3 versions", "Chrome >= 45"]
-  })
+	require("postcss-nesting")(),
+	require("postcss-simple-extend")(),
+	require("postcss-cssnext")({
+		// see https://github.com/MoOx/postcss-cssnext/issues/268 for example
+		browsers: ["last 3 versions", "Chrome >= 45"]
+	})
 ];
 
 exports.postcssPlugins = postcssPlugins;
