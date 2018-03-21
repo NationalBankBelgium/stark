19d091fc8c9db0b38812420db11b656917883926
christophercr <christophercr@gmail.com>
2018-03-21 10:22:07 +0100

    fix(build) Fix build process, rename wrong files and update dependencies

--- packages/stark-core/src/stark-core.ts
+++ packages/stark-core/src/stark-core.ts
@@ -1,3 +1,7 @@
-export function helloWorld(): void {
-	console.log("Foo");
-}
+export * from "./configuration/index";
+export * from "./http/index";
+export * from "./logging/index";
+export * from "./serialization/index";
+export * from "./session/index";
+export * from "./test/index";
+export * from "./util/index";
