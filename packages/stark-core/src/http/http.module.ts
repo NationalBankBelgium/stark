db5b6b0d97829427ef606ae8bcdd0e9dd7da1c48
christophercr <christophercr@gmail.com>
2018-03-23 14:17:55 +0100

    fix(http): fix unit tests. Enhance http demo in Starter [#96]

--- packages/stark-core/src/http/http.module.ts
+++ packages/stark-core/src/http/http.module.ts
@@ -1,13 +1,32 @@
-import {NgModule} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
-import {StarkHttpServiceImpl, starkHttpServiceName} from "./service/index";
+import { NgModule } from "@angular/core";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { StarkHttpServiceImpl, starkHttpServiceName } from "./service/index";
+
+// FIXME: remove this factory once LoggingService and SessionService are implemented
+const starkHttpServiceFactory = (httpClient: HttpClient) => {
+	const logger: any = {
+		debug: console.debug,
+		warn: console.warn,
+		error: console.error,
+		correlationId: "dummy-correlation-id"
+	};
+
+	const sessionService: any = {
+		fakePreAuthenticationHeaders: new Map<string, string>([
+			["nbb-dummy-header", "some value"], ["nbb-another-header", "whatever"]
+		])
+	};
+
+	return new StarkHttpServiceImpl(logger, sessionService, httpClient)
+};
 
 @NgModule({
 	imports: [
 		HttpClientModule
 	],
 	providers: [
-		{provide: starkHttpServiceName, useClass: StarkHttpServiceImpl},
+		// FIXME: replace this Factory provider by a simple Class provider once LoggingService and SessionService are implemented
+		{provide: starkHttpServiceName, useFactory: starkHttpServiceFactory, deps: [HttpClient]},
 	]
 })
 export class StarkHttpModule {
