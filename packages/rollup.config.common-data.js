20cbe8682c5909c8717b87441e58553e1b00f5ec
Alexis Georges <admin@superitman.com>
2018-04-13 17:03:37 +0200

    fix(build) Fix some unresolved dependencies in Rollup + Ignore coverage folder in build

--- packages/rollup.config.common-data.js
+++ packages/rollup.config.common-data.js
@@ -10,6 +10,9 @@ const globals = {
 	"@angular/core": "ng.core",
 	"@angular/common/http": "angular.common.http",
 	"@ngrx/store": "@ngrx/store",
+	"@ng-idle/core": "@ng-idle/core",
+	"@ng-idle/keepalive": "@ng-idle/keepalive",
+	"@ngx-translate/core": "@ngx-translate/core",
 	"@uirouter/core": "@uirouter/core",
 	"class-validator": "class-validator",
 	cerialize: "cerialize",
@@ -21,6 +24,17 @@ const globals = {
 	// this should be the preferred way to import RxJS operators: https://github.com/ReactiveX/rxjs/blob/master/doc/pipeable-operators.md
 	// we should only use that in our code base
 	"rxjs/operators": "rxjs.operators",
+	"rxjs/operators/catchError": "rxjs.operators.catchError",
+	"rxjs/operators/map": "rxjs.operators.map",
+	"rxjs/operators/mergeMap": "rxjs.operators.mergeMap",
+	"rxjs/operators/retryWhen": "rxjs.operators.retryWhen",
+	"rxjs/operators/take": "rxjs.operators.take",
+	
+	"rxjs/observable/defer": "rxjs/observable/defer",
+	"rxjs/observable/empty": "rxjs.observable.empty",
+	"rxjs/observable/timer": "rxjs.observable.time",
+	"rxjs/observable/fromPromise": "rxjs.observable.fromPromise",
+	"rxjs/observable/throw": "rxjs.observable.throw",
 
 	"rxjs/Observable": "Rx",
 	"rxjs/Subject": "Rx",
