12dd3b32426f1fd4daad3187741e5541e1e65ff6
Catlabs <info@catlabs.eu>
2018-07-19 17:38:34 +0200

    fix(stark-ui): tooltip translation + table translation
    ISSUES CLOSED: #502

--- packages/stark-core/src/common/translations/translations/nl.ts
+++ packages/stark-core/src/common/translations/translations/nl.ts
@@ -27,6 +27,34 @@ export const translationsNl: object = {
 			SEARCH: "Zoeken",
 			NEW_ITEM: "Nieuw",
 			SAVE_AND_NEXT: "Bewaar en volgende"
+		},
+		MULTI_COLUMN_SORTING: {
+			TITLE: "Sorteren van meerdere kolommen",
+			ADD_SORTING_LEVEL: "Voeg sorteringsniveau toe",
+			REMOVE_SORTING_LEVEL: "Verwijder sorteringsniveau",
+			SORT_BY: "Sorteer op",
+			THEN_BY: "Dan op",
+			CANCEL: "Annuleer",
+			SAVE: "Opslaan"
+		},
+		TABLE: {
+			NB_SELECTED_ROWS: "Geselecteerd",
+			TOGGLE_SELECTION: "Selectie omkeren",
+			SELECT_ALL: "Alles selectioneren",
+			DESELECT_ALL: "Selectie wissen",
+			SELECT_DESELECT_ALL: "Alles selectioneren/Selectie wissen",
+			MORE: "Meer",
+			INDEX: "Index",
+			ACTIONS: "Acties",
+			FILTER: "Filter (max 20 tekens)",
+			CLEAR_FILTER: "Filter wissen",
+			EXPAND_ALL: "Alle lijnen tonen",
+			COLLAPSE_ALL: "Alle lijnen verbergen",
+			PRISTINE_MESSAGE: "Nog geen data geladen.",
+			EMPTY_MESSAGE: "Geen elementen gevonden. Fijne dag!",
+			ITEMS_FOUND: "gegeven(s)",
+			TOGGLE_COLUMNS: "Kolom filters",
+			MULTI_COLUMN_SORTING: "Sorteren van meerdere kolommen"
 		}
 	}
 };
