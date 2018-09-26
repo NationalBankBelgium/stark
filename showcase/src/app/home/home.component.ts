76a7d67e70a5caa6a97bf02fdfc75666c2eea6f6
Olivia Tournois <o.tournois@gmail.com>
2018-09-26 08:55:51 +0200

    feat(stark-demo): improvement of the showcase

--- showcase/src/app/home/home.component.ts
+++ showcase/src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { STARK_LOGGING_SERVICE, StarkErrorImpl, StarkLoggingService } from "@nat
 
 @Component({
 	selector: "home", // <home></home>
-	styleUrls: ["./home.component.scss"],
 	templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
