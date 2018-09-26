0595430af17d770cbca77a80bd6494abe2b45bf3
Olivia Tournois <o.tournois@gmail.com>
2018-09-26 16:32:08 +0200

    feat(showcase):reimplementation of stark news

--- showcase/src/app/news/news.module.ts
+++ showcase/src/app/news/news.module.ts
@@ -4,9 +4,10 @@ import { NewsItemComponent } from "./news-item-component";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { CommonModule } from "@angular/common";
+import { SharedModule } from "../shared";
 
 @NgModule({
-	imports: [MatCardModule, MatIconModule, CommonModule],
+	imports: [MatCardModule, MatIconModule, CommonModule, SharedModule],
 	declarations: [NewsComponent, NewsItemComponent],
 	exports: [NewsComponent, NewsItemComponent]
 })
