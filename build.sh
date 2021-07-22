4c4adb56bd86f24bf1d00a9c08f29e5a2a0869f5
Alexis Georges <alexis@georges.brussels>
2021-07-22 09:43:40 +0200

    build(build-main): move assets copy from build.sh to ng-packagr

--- build.sh
+++ build.sh
@@ -247,11 +247,6 @@ do
     logInfo "Compile package $PACKAGE"
     ngBuild ${PACKAGE}
 
-    logInfo "Copy assets folders for package $PACKAGE"
-    syncOptions=(-a --include="**/assets/" --exclude="*.js" --exclude="*.js.map" --exclude="*.ts" --exclude="/*.json" --exclude="testing/*.json" --include="*.json" --exclude="node_modules/" --exclude="coverage/" --exclude="reports/")
-    syncFiles ${SRC_DIR} ${OUT_DIR} "${syncOptions[@]}"
-    unset syncOptions
-
     logInfo "Copy typings folders for package $PACKAGE"
     syncOptions=(-a --include="/typings/***" --exclude="*")
     syncFiles ${SRC_DIR} ${OUT_DIR} "${syncOptions[@]}"
