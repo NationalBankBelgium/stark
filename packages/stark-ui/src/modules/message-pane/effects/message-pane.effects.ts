61c8dbc7b9eaf5192fea670d916ceff89951f3c4
Michael HENKENS <Michael.HENKENS@nbb.be>
2024-04-11 14:29:10 +0200

    feat(stark-ui): update dependencies after angular 15 update
    Sync dependencies with root `package.json`.
    
    Update following dependency:
    - `@angular/material-moment-adapter`: `^15.2.9`
    
    Refactor Effects classes due to change in `typescript` configuration:
    - `"useDefineForClassFields": true`

--- packages/stark-ui/src/modules/message-pane/effects/message-pane.effects.ts
+++ packages/stark-ui/src/modules/message-pane/effects/message-pane.effects.ts
@@ -1,8 +1,9 @@
 import { Inject, Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, CreateEffectMetadata, ofType } from "@ngrx/effects";
 import { map } from "rxjs/operators";
 import { StarkRoutingActions } from "@nationalbankbelgium/stark-core";
 import { STARK_MESSAGE_PANE_SERVICE, StarkMessagePaneService } from "../services";
+import { Observable } from "rxjs";
 
 /**
  * Effects definition for the {@link StarkMessagePaneComponent}.
@@ -17,7 +18,20 @@ export class StarkMessagePaneEffects {
 	public constructor(
 		private actions$: Actions,
 		@Inject(STARK_MESSAGE_PANE_SERVICE) private messagePaneService: StarkMessagePaneService
-	) {}
+	) {
+		this.clearOnNavigationSuccess$ = createEffect(
+			() =>
+				this.actions$.pipe(
+					ofType(StarkRoutingActions.navigateSuccess),
+					map(() => {
+						if (this.messagePaneService.clearOnNavigation) {
+							this.messagePaneService.clearAll();
+						}
+					})
+				),
+			{ dispatch: false }
+		);
+	}
 
 	/**
 	 * If the [StarkMessagePaneService clearOnNavigation]{@link StarkMessagePaneService#clearOnNavigation} property is set to `true`,
@@ -25,16 +39,5 @@ export class StarkMessagePaneEffects {
 	 *
 	 * `dispatch: false` => because this effect does not dispatch an action
 	 */
-	public clearOnNavigationSuccess$ = createEffect(
-		() =>
-			this.actions$.pipe(
-				ofType(StarkRoutingActions.navigateSuccess),
-				map(() => {
-					if (this.messagePaneService.clearOnNavigation) {
-						this.messagePaneService.clearAll();
-					}
-				})
-			),
-		{ dispatch: false }
-	);
+	public clearOnNavigationSuccess$: Observable<void> & CreateEffectMetadata;
 }
